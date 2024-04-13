5905adf183aa0672f59173c287e3976a8afa52b0
DAMILOLA <dammylola299@gmail.com>
2024-04-13 22:20:43 +0100

    updates to singly-linked-list

--- DSA-COURSE-FROM-SCRATCH/4-data-structures/2-singly-linked-list.js
+++ DSA-COURSE-FROM-SCRATCH/4-data-structures/2-singly-linked-list.js
@@ -1,3 +1,10 @@
 // ============= SINGLY LINKED LIST =================
 
 // === what is a singly linked list? ===
+// A linked list is a data structure that contains a head, tail and length property.
+// Linked list consists of nodes, and each node has a value and a pointer to
+// another node or null.
+
+// Note: It's different from an array that has elements mapped to a particular index...
+// Here, we are dealing with nodes(akin to elements in an array) and we are not mapping to
+// any index, rather what we have is like a train of cars(nodes) connected to each other.
