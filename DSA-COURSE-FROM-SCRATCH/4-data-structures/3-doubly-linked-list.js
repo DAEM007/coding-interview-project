8017b5ebb9442c44252c80f31ca63f2013c31f80
DAMILOLA <dammylola299@gmail.com>
2024-04-17 23:04:51 +0100

    updates to doubly-linked-list

--- DSA-COURSE-FROM-SCRATCH/4-data-structures/3-doubly-linked-list.js
+++ DSA-COURSE-FROM-SCRATCH/4-data-structures/3-doubly-linked-list.js
@@ -1,1 +1,23 @@
 // ============= DOUBLY LINKED LIST =================
+
+// === what is a doubly linked list? ===
+// It is almost identical to a singly linked-list only that...here each node has another pointer to the
+// previous node.
+// In comparison to singly-linked list. It takes up more memory.
+// It's almost like a trade off...More memory === more flexibility
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = null;
+  }
+}
