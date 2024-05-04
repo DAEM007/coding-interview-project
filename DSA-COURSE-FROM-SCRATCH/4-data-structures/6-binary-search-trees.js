d47be6ceed29e42f485c6537523f0b607d019e71
DAMILOLA <dammylola299@gmail.com>
2024-05-04 01:22:35 +0100

    updates to binary-search-trees

---
+++ DSA-COURSE-FROM-SCRATCH/4-data-structures/6-binary-search-trees.js
@@ -0,0 +1,28 @@
+// ============= BINARY SEARCH TREES =================
+
+// === what is a  trees? ===
+
+// A tree is a data structure that consist of nodes in a parent-child relationship.
+
+// Note: one difference between list and trees is that: list are linear just as we saw in the linked list
+// chapter but trees on the other-hand are non-linear.
+
+// Note: A singly linked list can be a special case of a tree.
+
+// ====== common tree terminologies ======
+
+// Root: A top node in a tree.
+
+// Child: A node directly connected to another node.
+
+// Parent: A converse notion of a child (meaning that, it's same with the child but a reversed case).
+
+// Siblings: A group of nodes with same parent.
+
+// Leaf: A node with no child.
+
+// Edge: connection between one node and another.
+
+// ====== Real-world application of trees =======
+
+//
