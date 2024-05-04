e3f705126832fc8173d2d2e7248037dd68aad558
DAMILOLA <dammylola299@gmail.com>
2024-05-04 22:32:37 +0100

    updates to binary-search-trees

--- DSA-COURSE-FROM-SCRATCH/4-data-structures/6-binary-search-trees.js
+++ DSA-COURSE-FROM-SCRATCH/4-data-structures/6-binary-search-trees.js
@@ -1,6 +1,6 @@
 // ============= BINARY SEARCH TREES =================
 
-// === what is a  trees? ===
+// === what is a  tree? ===
 
 // A tree is a data structure that consist of nodes in a parent-child relationship.
 
@@ -11,18 +11,44 @@
 
 // ====== common tree terminologies ======
 
-// Root: A top node in a tree.
+// 1. Root: A top node in a tree.
 
-// Child: A node directly connected to another node.
+// 2. Child: A node directly connected to another node.
 
-// Parent: A converse notion of a child (meaning that, it's same with the child but a reversed case).
+// 3. Parent: A converse notion of a child (meaning that, it's same with the child but a reversed case).
 
-// Siblings: A group of nodes with same parent.
+// 4. Siblings: A group of nodes with same parent.
 
-// Leaf: A node with no child.
+// 5. Leaf: A node with no child.
 
-// Edge: connection between one node and another.
+// 6. Edge: connection between one node and another.
 
 // ====== Real-world application of trees =======
 
-//
+// 1. Html DOM.
+
+// 2. Network routing.
+
+// 3. Abstract syntax tree.
+
+// 4. Artificial intelligence.
+
+// 5. Folders in an operating system.
+
+// 6. computer file systems.
+
+// 7. JSON.
+
+// ==== Kinds of trees ====
+
+// There are a lot of trees but we would be focusing on:
+
+// 1. Trees - each nodes can have as many children as possible.
+
+// 2. Binary Trees - each nodes can have at most two (binary) children.
+
+// 3. Binary search Trees (BST) - This is a special case of a binary tree...more importantly they are sorted in a particular way...actually in an ordered way.
+
+// ==== Binary search trees - deep dive ====
+
+// Binary search trees are sorted such that... every node to the left of the parent node is always less than the parent...while every node to the right of the parent node is greater than the parent.
