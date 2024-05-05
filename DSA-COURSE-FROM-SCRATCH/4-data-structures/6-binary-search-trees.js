cdd563e3abc6c1ab4e6c6f2f0282aeca76330c06
DAMILOLA <dammylola299@gmail.com>
2024-05-05 23:50:30 +0100

    updates to binary-search-tree

--- DSA-COURSE-FROM-SCRATCH/4-data-structures/6-binary-search-trees.js
+++ DSA-COURSE-FROM-SCRATCH/4-data-structures/6-binary-search-trees.js
@@ -65,6 +65,42 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+  insert(val) {
+    let newNode = new Node(val);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      let current = this.root;
+      while (true) {
+        // this handles duplicates...unless you get into an infinite loop
+        if (val === current.val) return undefined;
+        if (val < current.val) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (val > current.val) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
 }
 
 let tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(5);
+tree.insert(13);
+tree.insert(11);
+tree.insert(2);
+tree.insert(16);
+tree.insert(7);
+console.log(tree);
