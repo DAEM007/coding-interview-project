761a9163d4080320714803b35291232bda21fdfb
DAMILOLA <dammylola299@gmail.com>
2024-05-10 18:45:12 +0100

    updates to binary-tree-traversal

--- DSA-COURSE-FROM-SCRATCH/4-data-structures/7-binary-tree-traversal.js
+++ DSA-COURSE-FROM-SCRATCH/4-data-structures/7-binary-tree-traversal.js
@@ -90,6 +90,17 @@ class BinarySearchTree {
     traverse(this.root);
     return data;
   }
+  DFSPostorder() {
+    let data = [];
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      data.push(node.val);
+      return;
+    }
+    traverse(this.root);
+    return data;
+  }
 }
 
 let tree = new BinarySearchTree();
@@ -101,3 +112,4 @@ tree.insert(8);
 tree.insert(20);
 console.log(tree.BFS());
 console.log(tree.DFSPreorder());
+console.log(tree.DFSPostorder());
