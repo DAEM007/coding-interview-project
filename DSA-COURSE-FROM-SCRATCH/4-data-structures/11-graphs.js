9a8b70ea1503b16ede881ef955dc46c25fa80a79
DAMILOLA <dammylola299@gmail.com>
2024-05-29 07:41:37 +0100

    updates to graphs

---
+++ DSA-COURSE-FROM-SCRATCH/4-data-structures/11-graphs.js
@@ -0,0 +1,14 @@
+// ============= GRAPHS =================
+
+// === what are graphs? ===
+// A graph data structure is simply a collection of nodes(vertices or points) and connections.
+// They could either be DIRECTED OR UNDIRECTED.
+
+// === Applications of graph ===
+// 1. social networks.
+// 2. Location/Mapping.
+// 3. Routing Algorithms.
+// 4. visual hierarchy.
+// 5. File system optimizations.
+
+// === Essential graph terms ===
