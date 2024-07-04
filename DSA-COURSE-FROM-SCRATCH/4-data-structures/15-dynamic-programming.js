060a8215979a28292b775ef46144661e8eb0e135
DAMILOLA <dammylola299@gmail.com>
2024-07-05 00:03:33 +0100

    updates to dynamic-programming

--- DSA-COURSE-FROM-SCRATCH/4-data-structures/15-dynamic-programming.js
+++ DSA-COURSE-FROM-SCRATCH/4-data-structures/15-dynamic-programming.js
@@ -7,3 +7,19 @@
     a collection of simpler sub-problems, solving each of those sub-problems just once, and 
     storing their solutions.
 */
+
+// === when can we use dynamic programming? ===
+
+/*
+    We can use dynamic programming when there are:
+    1. optimal sub-structure present.
+    2. overlapping sub-problems.
+*/
+
+// === overlapping sub-problems ===
+
+/*
+    A problem is said to have overlapping sub-problem if it can be broken down into
+    sub-problems which are re-used several times. A very good example is the fibonacci 
+    sequence.
+*/
