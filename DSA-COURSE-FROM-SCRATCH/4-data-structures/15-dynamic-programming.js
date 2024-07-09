38741b978186d2018511da90a0acfbf043f31d3f
DAMILOLA <dammylola299@gmail.com>
2024-07-09 17:00:21 +0100

    updates to dynamic-programming

--- DSA-COURSE-FROM-SCRATCH/4-data-structures/15-dynamic-programming.js
+++ DSA-COURSE-FROM-SCRATCH/4-data-structures/15-dynamic-programming.js
@@ -38,10 +38,54 @@
     "Those who cannot remember the past are condemned to repeat it"  - :)
 */
 
+// Now, let's take a simple fibonacci sequence recursive function for example...
+
+/*
+    Note: You shouldn't try to run the code below with numbers above 40...cos it gets really slow at
+    that point and can slow your browser if you are using that to run the code.
+*/
+
+function fib(n) {
+  if (n <= 2) return 1;
+  return fib(n - 1) + fib(n - 2);
+}
+
+// console.log(fib(5));
+
+/*
+    Note: One thing to note about this function is how it can get very slow pretty quickly...
+    The question is why?
+    The reason is simple...You would realize that as n grows, the recursive function performs 
+    many repetitive calls again and again even though they have been computed before...which 
+    causes the function to get really slow as there's no place to store/cache function calls 
+    that have been computed before for res-usability. 
+    Now, this particular reason is what would bring us into the concept of "MEMOIZATION".
+*/
+
 // === Introduction to Memoization ===
 
 // === what is memoization? ===
 
 /*
-    
+    Memoization is simply the idea of storing expensive function calls and returning the cached
+    result when the same inputs occur again.
+*/
+
+function fibMemoized(n, memo = []) {
+  if (memo[n] !== undefined) return memo[n];
+  if (n <= 2) return 1;
+  let res = fibMemoized(n - 1, memo) + fibMemoized(n - 2, memo);
+  memo[n] = res;
+  return res;
+}
+
+// console.log(fibMemoized(100));
+
+/*
+    Note: Do you observe how faster the algorithm became by utilizing memoization...You can now
+    run figures like even 50, 70 and 100 cos we are not repeating computations we have already done
+    before.
+
+    Now, the quote we mentioned earlier makes sense, we are not condemned to repeat the past cos we
+    can remember it...You can now see how memoization helps us in dynamic programming.
 */
